7a1e3b38c583fa998a6dc63b136a6a488db0eb7d
Eli <eli.sundance.baker@gmail.com>
2019-08-08 16:50:15 -0400

    Cookie is evaluating to true, popup is working

--- template-parts/blocks/t2-apply-visit-request/apply-visit-request.js
+++ template-parts/blocks/t2-apply-visit-request/apply-visit-request.js
@@ -14,31 +14,62 @@
      */
     var initializeBlock = function( $block ) {
         //alert('apply-visit-request block initialized');
-        // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-document.onload = function() {
-  modal.style.display = "block";
+        //  Set the Cookie
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+// Get the Cookie
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca   = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var  c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if ( c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+// Check if Cookie exists
+function checkCookie() {
+
+    // Get the cookie called "visited"
+    var visited = getCookie("visited");
+
+    // If it exists, print the Cookie to the Console
+    if (visited == "true") {
+        console.log(document.cookie);
+    }
+    else {
+        // If not, add the class 'is-visible' to my modal called '.mhc-intro-modal'
+        // and create a the cookie "visited=true" expiring in 15 days.
+        $('.mhc-intro-modal').addClass('is-visible');
+        $('body').addClass('hide-overflow');
+        setCookie("visited", "true", 15);
+    }
 }
+
+// When document is ready check for the cookie
+$(document).ready(function() {
+    checkCookie();
+});
+
+// Close the modal
+$('.mhc-intro-modal').on('click', function(event) {
+    if ( $(event.target).is('.mhc-intro-modal-close') || $(event.target).is('.mhc-intro-modal') ) {
+        event.preventDefault();
+        $(this).removeClass('is-visible');
+        $('body').removeClass('hide-overflow');
+    }
+});
         
     }
 
